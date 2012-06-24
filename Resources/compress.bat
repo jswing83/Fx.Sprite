fb227f25adedb0c7008f6a1125cde73168981359
Jason Swing <jswing83>
2012-06-24 16:51:05 -0400

    added yui compression scripts

---
+++ Resources/compress.bat
@@ -0,0 +1,1 @@
+java -jar yuicompressor-2.4.7.jar -o ../Source/Fx.Sprite-yc.js ../Source/Fx.Sprite.js
---
+++ Resources/compress.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar yuicompressor-2.4.7.jar -o ../Source/Fx.Sprite-yc.js ../Source/Fx.Sprite.js
